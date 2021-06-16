47e0c443cefcf93b7d08d7a0b86e0cb76217fbd0
Pavel Krasnov <https://github.com/Billiardist94/if-html.git>
2021-06-16 12:53:27 +0300

    lesson-14

--- main.js
+++ main.js
@@ -1,69 +1,15 @@
-//___<section class="homes-guests-loves">___
-const homesGuestsLovesSection = document.createElement('section');
-homesGuestsLovesSection.classList.add('homes-guests-loves');
-const body = document.body;
-body.appendChild(homesGuestsLovesSection);
+const {form} = document.forms;
 
-//___<div class="container">___
-const divContainer = document.createElement('div');
-divContainer.classList.add('container');
-homesGuestsLovesSection.appendChild(divContainer);
-
-//___<div class="homes-block-header">___
-const divHeader = document.createElement('div');
-divHeader.classList.add('homes-block-header');
-divContainer.appendChild(divHeader);
-//___<h3 class="h3-text">Homes guests loves___
-const h3 = document.createElement('h3');
-h3.classList.add('h3-text');
-h3.innerText = 'Homes guests loves';
-divHeader.insertAdjacentElement('afterbegin', h3);
-
-//___<div class="homes-block-cards">___
-const divCards = document.createElement('div');
-divCards.classList.add('homes-block-cards');
-divHeader.insertAdjacentElement('afterend', divCards)
-
-//___<ul class="homes-list">___
-const ul = document.createElement('ul')
-ul.classList.add("homes-list");
-divCards.appendChild(ul)
-
-//___<div class="homes-list-card">___
-const generateCard = (name, city, country, imageUrl) => {
-    return `
-        <li class="homes-list-item col-3 col-md-6 col-sm-3">
-            <div class="homes-list-card">
-                <div class="homes-list-images">
-                    <img src=${imageUrl} class="homes-link-image" alt='${name}'>
-                </div>
-                <div class="homes-list-content">
-                    <div class="homes-list-heading">
-                        <span class="homes-list-title">${name}</span>
-                        <span class="homes-list-subtitle">${city}, ${country}</span>
-                    </div>
-                </div>
-            </div>
-        </li>
-    `;
+const getFormValues = function (event) {
+    event.preventDefault();
+    const formData = new FormData(form);
+    fetch('https://fe-student-api.herokuapp.com/api/file', {
+        method: 'POST',
+        body: formData,
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
 }
-
-if (sessionStorage.getItem('homesList') !== null) {
-    let homesListCardsHTML = JSON.parse(sessionStorage.homesList).map(card => {
-        return generateCard(card.name, card.city, card.country, card.imageUrl)
-    }).join('')
-    ul.innerHTML = homesListCardsHTML;
-}
-else {
-    async function getContent(url) {
-        const response = await fetch(url);
-        const json = await response.json();
-        const sesStorage = sessionStorage;
-        sesStorage.homesList = JSON.stringify(json)
-        let homesListCardsHTML = JSON.parse(sesStorage.homesList).map(card => {
-            return generateCard(card.name, card.city, card.country, card.imageUrl)
-        }).join('')
-        ul.innerHTML = homesListCardsHTML;
-    }
-    getContent('https://fe-student-api.herokuapp.com/api/hotels/popular')
-}
+form.addEventListener('submit', getFormValues)
